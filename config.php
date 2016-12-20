21e88248bc0855015fcb821e9651305fd3b96333
Murviet <154199219@qq.com>
2016-12-20 17:34:24 +0800

    config.php

---
+++ config.php
@@ -0,0 +1,35 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/wap_shop_legoods_bak/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/wap_shop_legoods_bak/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/wamp/www/wap_shop_legoods_bak/catalog/');
+define('DIR_SYSTEM', 'D:/wamp/www/wap_shop_legoods_bak/system/');
+define('DIR_LANGUAGE', 'D:/wamp/www/wap_shop_legoods_bak/catalog/language/');
+define('DIR_TEMPLATE', 'D:/wamp/www/wap_shop_legoods_bak/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/wamp/www/wap_shop_legoods_bak/system/config/');
+define('DIR_IMAGE', 'D:/wamp/www/wap_shop_legoods_bak/image/');
+define('DIR_CACHE', 'D:/wamp/www/wap_shop_legoods_bak/system/cache/');
+define('DIR_DOWNLOAD', 'D:/wamp/www/wap_shop_legoods_bak/system/download/');
+define('DIR_UPLOAD', 'D:/wamp/www/wap_shop_legoods_bak/system/upload/');
+define('DIR_MODIFICATION', 'D:/wamp/www/wap_shop_legoods_bak/system/modification/');
+define('DIR_LOGS', 'D:/wamp/www/wap_shop_legoods_bak/system/logs/');
+define('APP_PAYMENT_HOST', 'http://test.boccfc.cn/paygate/service.xml');
+define('APP_LOGIN_URL' , 'http://test.boccfc.cn/haitao/apply/preApply.html');
+define('APP_APILOGIN_URL' , 'http://test.boccfc.cn/haitao/apply/unionlogin.json');
+define('APP_MERCHANTID', 'N03020100000000');
+define('APP_MERCHANTCODE', 'N03020100000010');
+define('DOLLAR_RATE', 6.2);
+define('TAX_DISCOUNT',0.7);
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '192.168.7.11');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123.com');
+define('DB_DATABASE', 'wap_shop_legoods');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'sl_');
